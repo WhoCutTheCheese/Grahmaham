963dccf92c368c22d6d0de620628d11afe3bac5c
WhoCutTheCheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2021-03-31 22:14:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const mongoose = require('mongoose');
 const version = "ALPHA-1.0.1";
+const Guild = require("./models/prefix");
 bot.on('ready', () => {
     console.log(`Grahmaham Version: ${version} is starting...`);
     console.log(` `)
@@ -19,7 +20,45 @@ bot.on('message', message => {
     let args = message.content.substring(prefix.length).split(" ");
     switch (args[0]) {
         case "prefix":
+            if (!message.member.hasPermission('MANAGE_GUILD')) {
+                return message.channel.send('You do not have permission to use this command!').then(m => m.delete({
+                    timeout: 10000
+                }));
+            };
 
+            const settings = Guild.findOne({
+                guildID: message.guild.id
+            }, (err, guild) => {
+                if (err) console.error(err)
+                if (!guild) {
+                    const newGuild = new Guild({
+                        _id: mongoose.Types.ObjectId(),
+                        guildID: message.guild.id,
+                        guildName: message.guild.name,
+                        prefix: process.env.PREFIX
+                    })
+
+                    newGuild.save()
+                        .then(result => console.log(result))
+                        .catch(err => console.error(err));
+
+                    return message.channel.send('This server was not in our database! We have added it, please retype this command.').then(m => m.delete({
+                        timeout: 10000
+                    }));
+                }
+            });
+
+            if (args.length < 1) {
+                return message.channel.send(`You must specify a prefix to set for this server! Your current server prefix is \`${settings.prefix}\``).then(m => m.delete({
+                    timeout: 10000
+                }));
+            };
+
+            settings.updateOne({
+                prefix: args[0]
+            });
+
+            return message.channel.send(`Your server prefix has been updated to \`${args[0]}\``);
     }
 })
 
