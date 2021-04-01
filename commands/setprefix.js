2b319c4c95d50ea525944da06ebe76cad75ee781
WhoCutTheCheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2021-04-01 12:29:04 -0500

    Handler
    Basic handler created.

---
+++ commands/setprefix.js
@@ -0,0 +1,64 @@
+const prefix = require("../models/prefix")
+
+module.exports = {
+    name: 'prefix',
+    description: "Set your server's prefix.",
+    run: async (bot, message, args) => {
+        if (!message.member.hasPermission('MANAGE_GUILD')) {
+            return message.channel.send(noPermissions).then(m => m.delete({
+                timeout: 10000
+            }));
+        };
+
+        const settings = await Guild.findOne({
+            guildID: message.guild.id
+        }, (err, guild) => {
+            if (err) console.error(err)
+            if (!guild) {
+                const newGuild = new Guild({
+                    _id: mongoose.Types.ObjectId(),
+                    guildID: message.guild.id,
+                    guildName: message.guild.name,
+                    prefix: "!!"
+                })
+
+                newGuild.save()
+                    .then(result => console.log(result))
+                    .catch(err => console.error(err));
+
+                return message.channel.send('This server was not in our database! We have added it, please retype this command.').then(m => m.delete({
+                    timeout: 10000
+                }));
+            }
+        });
+
+        if (!args[1]) {
+            const noPrefix = new Discord.MessageEmbed()
+                .setTitle("Prefix")
+                .setColor("RED")
+                .setDescription("Change the prefix for your server.")
+                .addField("Current Prefix:", `\`${settings.prefix}\``)
+                .addField("Change Prefix:", `\`${settings.prefix}prefix <New Prefix>\``)
+                .setTimestamp()
+            return message.channel.send(noPrefix).then(m => m.delete({timeout: 10000}));
+        };
+        if (args[1].length > 3) {
+            const tooLong = new Discord.MessageEmbed()
+                .setDescription(":x: This prefix is too long, please choose another.")
+                .setColor("RED")
+            return message.channel.send(tooLong).then(m => m.delete({
+                timeout: 10000
+            }));
+        }
+
+        await settings.updateOne({
+            prefix: args[1]
+        });
+        const setPrefix = new Discord.MessageEmbed()
+            .setTitle("Prefix")
+            .setColor("GREEN")
+            .addField("Prefix Updated", `Your guild's prefix has been updated to \`${args[1]}\``)
+            .setTimestamp()
+        return message.channel.send(setPrefix);
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const fs = require('fs');
 const mongoose = require('mongoose');
 const version = "ALPHA-1.0.2";
 const Guild = require("./models/prefix");
@@ -10,8 +11,15 @@ bot.on('ready', () => {
     bot.user.setActivity(`${version} | !!help`, {
         type: "STREAMING",
         url: "https://www.twitch.tv/WhoCutTehCheese"
-    });
-})
+    })
+});
+bot.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    bot.commands.set(command.name, command);
+}
 const noPermissions = new Discord.MessageEmbed()
     .setDescription(":x: You do not have the required permission to use this command.")
     .setColor("RED")
@@ -36,60 +44,7 @@ bot.on('message', async message => {
     let args = message.content.substring(prefix.length).split(" ");
     switch (args[0]) {
         case "prefix":
-            if (!message.member.hasPermission('MANAGE_GUILD')) {
-                return message.channel.send(noPermissions).then(m => m.delete({
-                    timeout: 10000
-                }));
-            };
-
-            const settings = await Guild.findOne({
-                guildID: message.guild.id
-            }, (err, guild) => {
-                if (err) console.error(err)
-                if (!guild) {
-                    const newGuild = new Guild({
-                        _id: mongoose.Types.ObjectId(),
-                        guildID: message.guild.id,
-                        guildName: message.guild.name,
-                        prefix: "!!"
-                    })
-
-                    newGuild.save()
-                        .then(result => console.log(result))
-                        .catch(err => console.error(err));
-
-                    return message.channel.send('This server was not in our database! We have added it, please retype this command.').then(m => m.delete({
-                        timeout: 10000
-                    }));
-                }
-            });
-
-            if (!args[1]) {
-                const noPrefix = new Discord.MessageEmbed()
-                    .setTitle("Prefix")
-                    .setColor("RED")
-                    .setDescription("Change the prefix for your server.")
-                    .addField("Current Prefix:", `\`${settings.prefix}\``)
-                    .addField("Change Prefix:", `\`${settings.prefix}prefix <New Prefix>\``)
-                    .setTimestamp()
-                return message.channel.send(noPrefix).then(m => m.delete({timeout: 10000}));
-            };
-            if (args[1].length > 3) {
-                const tooLong = new Discord.MessageEmbed()
-                    .setDescription(":x: This prefix is too long, please choose another.")
-                    .setColor("RED")
-                return message.channel.send(tooLong)
-            }
-
-            await settings.updateOne({
-                prefix: args[1]
-            });
-            const setPrefix = new Discord.MessageEmbed()
-                .setTitle("Prefix")
-                .setColor("GREEN")
-                .addField("Prefix Updated", `Your guild's prefix has been updated to \`${args[1]}\``)
-                .setTimestamp()
-            return message.channel.send(setPrefix);
+            bot.commands.get('prefix').run(bot, message, args);
     }
 })
 
