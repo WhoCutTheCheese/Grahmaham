8c6151ceb7af438d1253398e5920275aa8b6cc3b
WhoCutTheCheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2021-03-31 21:36:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const mongoose = require('mongoose');
-const version = "ALPHA-1.0.0";
+const version = "ALPHA-1.0.1";
 bot.on('ready', () => {
     console.log(`Grahmaham Version: ${version} is starting...`);
     console.log(` `)
