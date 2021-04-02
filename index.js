0e925d3d5e5ad41af45703672694e143c2815ebd
WhoCutTheCheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2021-04-02 01:08:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,10 @@ bot.on("guildCreate", async guild => {
 
 });
 bot.on('message', async message => {
-    if (!message.content.toLowerCase().startsWith(prefix) && message.content.toLowerCase().startsWith(`<@!${ClientID}>`)) { message.channel.send(`**PING!** My prefix for this server is: \`${prefix}\``); return;}
     const settings = await Guild.findOne({ guildID: message.guild.id })
     const prefix = `${settings.prefix}`;
+    if (!message.content.toLowerCase().startsWith(prefix) && message.content.toLowerCase().startsWith(`<@!${ClientID}>`)) { message.channel.send(`**PING!** My prefix for this server is: \`${prefix}\``); return;}
+
     if (!message.content.startsWith(prefix)) return;
 
 
